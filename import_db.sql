3b9f112008f49576acfc0e2d711577290e5b6c78
Justin <jcdiner@gmail.com>
2023-02-08 11:25:24 -0500

    Start Seed Data

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON; 
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -8,9 +10,9 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    users_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
 
-    FOREIGN KEY (users_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
@@ -33,3 +35,34 @@ CREATE TABLE replies (
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_likes (
+	id INTEGER PRIMARY KEY,
+	question_id INTEGER NOT NULL, 
+	user_id INTEGER NOT NULL, 
+
+	FOREIGN KEY (question_id) REFERENCES questions(id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+INSERT INTO  -- Users
+	users(fname, lname)
+VALUES 
+	('Justin', 'Diner'),
+	('Milner', 'Chen');
+
+INSERT INTO -- Questions
+	questions(title, body, user_id)
+VALUES
+	('How do you do SQL?', 'Help Please! I don''t know how to do homework!', (SELECT id FROM users WHERE fname = 'Milner')),
+	('Do you need to use LIKE or = for string comparison?', "Help Me Too! I'm lost!!", (SELECT id FROM users WHERE fname = 'Justin'));
+
+INSERT INTO 
+	question_follows(question_id, user_id)
+VALUES
+	((SELECT id FROM questions WHERE title = 'How do you do SQL?'), 
+	(SELECT id from users WHERE fname = 'Justin')),
+	((SELECT id FROM questions WHERE title = 'Do you need to use LIKE or = for string comparison?'), 
+	(SELECT id from users WHERE fname = 'Milner'));
+
+
